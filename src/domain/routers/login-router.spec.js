a5ff7941b7138d271f37f6b1b79b6b235aa79b16
Eric Oliveira <eriicsilva28@gmail.com>
2024-03-22 17:11:37 -0300

    feat: ensure loginRouter returns 500 if an invalid httpRequest is provided or httpResquest has no body

--- src/domain/routers/login-router.spec.js
+++ src/domain/routers/login-router.spec.js
@@ -1,6 +1,14 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500,
+      };
+    }
+
+    const { email, password } = httpRequest.body;
+
+    if (!email || !password) {
       return {
         statusCode: 400,
       };
@@ -20,5 +28,35 @@ describe("Login Router", () => {
     const httpResponse = sut.route(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
+  });
+  test("should return status 400 if no password is provided", () => {
+    const sut = new LoginRouter();
+    const httpRequest = {
+      body: {
+        email: "eric@dev.com",
+      },
+    };
+
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test("should return status 500 if no httpRequest is provided", () => {
+    const sut = new LoginRouter();
+
+    const httpResponse = sut.route();
+
+    expect(httpResponse.statusCode).toBe(500);
+  });
+
+  test("should return status 500 if no httpRequest has no body", () => {
+    const sut = new LoginRouter();
+
+    const httpRequest = {};
+
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
