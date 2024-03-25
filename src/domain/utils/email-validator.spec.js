2751bf879ee79061ca2ed32a022a909cdeb0083c
Eric Oliveira <eriicsilva28@gmail.com>
2024-03-25 16:43:27 -0300

    start test emailvalidator and move to utils

--- src/domain/utils/email-validator.spec.js
+++ src/domain/utils/email-validator.spec.js
@@ -1,14 +1,31 @@
-class EmailValidator {
-  isValid(email) {
-    return true;
-  }
-}
+const validator = require("validator");
+const EmailValidator = require("./email-validator");
+
+// factory
+const makeSut = () => {
+  return new EmailValidator();
+};
 
 describe("Email Validator", () => {
   test("should return true if validator returns true", () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid("valid_email@.com");
 
     expect(isEmailValid).toBe(true);
+  });
+
+  test("should return false if validator returns false", () => {
+    validator.isEmailValid = false;
+    const sut = makeSut();
+    const isEmailValid = sut.isValid("valid_email");
+
+    expect(isEmailValid).toBe(false);
+  });
+
+  test("should call with correct email", () => {
+    const sut = makeSut();
+    sut.isValid("valid_email");
+
+    expect(validator.email).toBe("valid_email");
   });
 });
